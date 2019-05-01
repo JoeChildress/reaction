733064c3a46fb364f8afb583f633e93befc2fb0d
Joseph Childress <josephchildress@Josephs-MacBook-Pro.local>
2019-04-30 18:56:24 -0700

    Overview edit

--- src/Components/Overview.js
+++ src/Components/Overview.js
@@ -6,9 +6,6 @@ class Overview extends Component {
         return <div>
             <p>A trivia game where you use your phone to text in your answers.</p>
             <p>Press the button below then send in your player name.</p>
-            {/* <button onClick={() => {
-						//this.props.removePost(this.props.index)
-                    }}> START </button> */}
                     <button>
                     <Link className="startIcon" to="Enter">START</Link>
                     </button>
